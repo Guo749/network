788b616fb7b049ba9d6d029efe45326bfcaaf306
Guo749 <venturekwok@gmail.com>
2021-12-14 09:36:23 -0500

    initialize

---
+++ cmake-build-debug/CMakeFiles/csapp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/csapp.dir/csapp.c.o"
+  "CMakeFiles/csapp.dir/main.c.o"
+  "csapp"
+  "csapp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/csapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
